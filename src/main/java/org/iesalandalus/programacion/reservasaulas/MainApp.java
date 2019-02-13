e8567b9de152e6bd9c8ddb197e8dc626e75dfadf
Manuel <Manuel@casa>
2019-02-13 14:21:11 +0100

    copio las clases del proyecto anterior

--- src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
@@ -1,9 +1,20 @@
+//proyecto final subido a github
 package org.iesalandalus.programacion.reservasaulas;
 
+import org.iesalandalus.programacion.reservasaulas.vista.IUTextual;
+
+
+//import org.iesalandalus.programacion.reservasaulas.vista
+
+
+
+        
 public class MainApp {
 
 	public static void main(String[] args) {
-		System.out.println("Programa para la gestión de reservas de espacios del IES Al-Ándalus");
+		System.out.println("Programa para la gestión de reservas de aulas");
+		IUTextual vista = new IUTextual();
+		vista.comenzar();
 	}
 
 }
